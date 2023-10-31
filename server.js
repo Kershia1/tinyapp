712f945b2962ef0cca2bbdaa9c54ce923ad6ff04
Kershia1 <insatiable.meerkat@gmail.com>
2023-10-31 15:46:43 +0000

    installed cookie parser, set as a require, added as app.use in server.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
